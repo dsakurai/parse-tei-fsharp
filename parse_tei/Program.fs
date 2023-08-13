f03a505e5943f7414390e8dc2bb0d48f9ee49860
Daisuke Sakurai <d.sakurai0@gmail.com>
2023-08-13 14:27:57 +0900

    Print some contents of an xml

--- parse_tei/Program.fs
+++ parse_tei/Program.fs
@@ -1,2 +1,17 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+open System
+open System.Xml.Linq
+
+let processXml (filePath: string) =
+    let xdoc = XDocument.Load(filePath)
+    xdoc.Descendants("message") // Replace with the name of the parent element
+        |> Seq.iter (fun parentElement ->
+            parentElement.Descendants("to") // Replace with the name of the grandchild element
+                |> Seq.iter (fun grandchildElement ->
+                    // Process each grandchild element here
+                 Console.WriteLine(grandchildElement.Value)
+                )
+        )
+        
+processXml "/Users/Shared/work/shared/code-dev/learn-german-fsharp/learn_german/resources/sample_xml/test.xml"
